0fd55fc3a543630fd56078fcbeb2aeb8dacc32f5
luandro <luandro@gmail.com>
2019-01-03 11:30:37 -0200

    begin adding seo

--- pages/issue.js
+++ pages/issue.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Link from 'next/link'
 import { withRouter } from 'next/router'
+import Head from 'next/head'
 import { Query, Mutation } from 'react-apollo'
 import App from '../components/App'
 import ISSUE from '../queries/issue.gql'
@@ -9,6 +10,7 @@ import Loading from '../components/Loading'
 class Issue extends Component {
     render () {
       const { router: { query: { key } } } = this.props
+      console.log(this.props)
       return (
         <App>
           <Query query={ISSUE} variables={{issueKey: key}}>
